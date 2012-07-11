110ff608f14cf53aea26d5299a0db4a2068e1e0e
Gerard (Gerry) Caulfield <githubProjects@caulfield.me>
2012-07-12 00:18:02 +0800

    Initial commit of my custom aliases file

---
+++ modules/gerry/init.zsh
@@ -0,0 +1,9 @@
+#
+# Defines custom aliases that I (Gerry) created for myself
+#
+# Authors:
+#   Gerard (Gerry) Caulfield <githubProjects@caulfield.me>
+#
+
+# Aliases
+alias ealiases='e ~/.oh-my-zsh/modules/gerry/init.zsh'
